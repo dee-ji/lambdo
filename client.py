bf0fc582a8dbdd2cea1be0f93fc50222c2ee92f5
dee-ji <deeji.0007@gmail.com>
2025-01-29 19:45:04 -0500

    Updated by_location arg to accept either the name or description field as a search pattern to find available instances

--- client.py
+++ client.py
@@ -41,18 +41,31 @@ def instances():
 
 @app.command()
 @click.argument("instance-types")
-def instance_types(gpu: str | None = None, available: bool = False):
+def instance_types(gpu: str | None = None, available: bool = False, by_location: str | None = None):
     # curl -u API-KEY: https://cloud.lambdalabs.com/api/v1/instance-types | jq .
     resp = get_response(url="https://cloud.lambdalabs.com/api/v1/instance-types")
 
     if gpu is not None:
         print(json.dumps(resp.json()["data"][gpu], indent=2))
-    if available:
+    elif available:
         find_available = [
             inst for inst in resp.json()["data"] if resp.json()["data"][inst]["regions_with_capacity_available"]
         ]
         if find_available:
             print(json.dumps(find_available, indent=2))
+        else:
+            print("There are currently no instances available...")
+    elif by_location is not None:
+        find_by_location = [
+            loc for inst in resp.json()["data"] for loc in resp.json()["data"][inst]["regions_with_capacity_available"]
+            if by_location.lower() in loc["description"].lower() or by_location.lower() in loc["name"]
+        ]
+        if find_by_location:
+            available_instance = [
+                resp.json()["data"][inst] for inst in resp.json()["data"]
+                if find_by_location[0] in resp.json()["data"][inst]["regions_with_capacity_available"]
+            ]
+            print(json.dumps(available_instance, indent=2))
         else:
             print("There are currently no instances available...")
     else:
