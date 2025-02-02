9ce686be3370fe657f5bf63800d8a755cba0b986
data <deeji.0007@gmail.com>
2025-02-01 20:02:46 -0500

    Added ssh-keys subcommand that currently lists all configured keys

--- lambdo/client.py
+++ lambdo/client.py
@@ -4,11 +4,12 @@ from typing import Annotated
 
 import typer
 from lambdo.lib.helpers import get_response
-from lambdo.sub_cmds import instance_types
+from lambdo.sub_cmds import instance_types, ssh_keys
 
 
 app = typer.Typer(help="Lambdo is a CLI built to utilize Lambda GPU Cloud Public APIs")
 app.add_typer(instance_types.app, name="instance-types", help="List the instances types offered by Lambda GPU Cloud")
+app.add_typer(ssh_keys.app, name="ssh-keys", help="Manage SSH Keys for your instances")
 
 
 def main():
