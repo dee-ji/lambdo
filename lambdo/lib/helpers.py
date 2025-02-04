7691adb51892756a1b5db2408e15e141b0fc29db
dee-ji <deeji.0007@gmail.com>
2025-02-03 19:40:20 -0500

    Updated error handling for post to allow for errors to be reported by Lambda Labs

--- lambdo/lib/helpers.py
+++ lambdo/lib/helpers.py
@@ -1,7 +1,7 @@
 import json
 import typer
 import requests
-from requests import Response
+from requests import Response, HTTPError
 from typing import Optional
 from lambdo.lib.settings import api_token
 
@@ -34,18 +34,17 @@ def post_request(
         if data is not None:
             data = json.dumps(data)
             response = requests.post(url=url, auth=(api_token, ""), data=data, headers=headers)
-            # print(json.dumps(response.json(), indent=2))
         elif files is not None:
             response = requests.post(url=url, auth=(api_token, ""), files=files, headers=headers)
-            # print(json.dumps(response.json(), indent=2))
         else:
             response = requests.post(url=url, auth=(api_token, ""), headers=headers)
-            # print(json.dumps(response.json(), indent=2))
     except requests.RequestException as e:
         typer.echo(f"Error fetching instance types: {e}")
         raise typer.Exit(code=1)
-
-    response.raise_for_status()
+    try:
+        response.raise_for_status()
+    except HTTPError:
+        typer.echo("There was an issue with your request. See the below error for more details.")
 
     return response
 
